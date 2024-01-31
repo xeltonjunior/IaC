dd6a307d5b2ed0884f6aedb914c338559622aec4
Elton Gomes <xeltonjunior@gmail.com>
2024-01-31 10:41:55 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-008fe2fc65df48dac"
+  instance_type = "t2.micro"
+  key_name = "iac-devops"
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
