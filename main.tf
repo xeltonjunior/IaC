8fc4cdef47baec2bf1442b22c9fa4a203d31ebba
Elton Gomes dos Santos Junior <96448725+xeltonjunior@users.noreply.github.com>
2024-02-02 11:45:41 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.35.0"
+      version = "~> 4.16"
     }
   }
 
@@ -10,15 +10,15 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-008fe2fc65df48dac"
+  ami =  "ami-008fe2fc65df48dac"
   instance_type = "t2.micro"
   key_name = "iac-devops"
 # user_data = <<-EOF
-# #!/bin/bash
+# !/bin/bash
 # cd /home/ubuntu
 # echo "<h1>Feito com Terraform</h1>" > index.html
 # nohup busybox httpd -f -p 8080 &
