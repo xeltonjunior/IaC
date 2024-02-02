5ac1991b3423275d6564440cd919bbe7012b5d93
Elton Gomes dos Santos Junior <96448725+xeltonjunior@users.noreply.github.com>
2024-02-02 11:43:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "5.35.0"
     }
   }
 
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-008fe2fc65df48dac"
   instance_type = "t2.micro"
   key_name = "iac-devops"
+# user_data = <<-EOF
+# #!/bin/bash
+# cd /home/ubuntu
+# echo "<h1>Feito com Terraform</h1>" > index.html
+# nohup busybox httpd -f -p 8080 &
+# EOF
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste aws"
   }
 }
